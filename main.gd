14d5beb106df44853cb4b984604453098488161c
Isabel Gutierrez <iguti@live.com>
2024-04-09 11:05:21 -0400

    Added more scenes, got the windows sized and displaying correctly, and added a little button on the sticky note while playing around.

--- main.gd
+++ main.gd
@@ -3,12 +3,11 @@ extends Node2D
 func _on_quit_pressed():
 	get_tree().quit()
 
+func _on_start_pressed():
+	var delay = Timer.new();
+	delay.wait_time = 2;
+	delay.autostart =true;
+	get_tree().change_scene_to_file("res://quiz.tscn")
 
-
-
-
-
-
-
-
-
+func _on_load_pressed():
+	pass
---
+++ quiz.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+func _on_sticky_note_pressed():
+	$TextureRect/StickyNote.text = "yeehaw"
+
