22471149cb366fefcca30c8f0f2d15dbe678bcfa
Isabel Gutierrez <iguti@live.com>
2024-04-09 21:05:28 -0400

    Added basic (if crappy) mockup of the quiz window, made the quiz buttons kind of work, and just kind of feeling my way around godot still

--- quiz.gd
+++ quiz.gd
@@ -3,5 +3,14 @@ extends Node2D
 
 
 func _on_sticky_note_pressed():
+	#should cycle through messages for fun
 	$TextureRect/StickyNote.text = "yeehaw"
+	
+func _on_quit_button_pressed():
+	#add save game warning
+	get_tree().quit()
 
+
+func _on_submit_pressed():
+	if(true):
+		print("yay")
